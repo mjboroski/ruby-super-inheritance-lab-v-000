cdac1c027c8353e560e7755d52c83391dd93c78c
Michael Boroski <mjboroski@gmail.com>
2018-04-26 06:04:27 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,9 +6,16 @@ class ChattyStudent<Student
   end
 
   def raise_hand
-    10.times do
-      super
-    end
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
     puts "Pick me!"
   end
 
